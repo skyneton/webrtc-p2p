61206f55c323280e4885b22b88ee65390bf582db
skyneton <67633420+skyneton@users.noreply.github.com>
2021-01-16 00:40:50 +0900

    Update main.js

--- router/main.js
+++ router/main.js
@@ -3,7 +3,7 @@ const session = require("./session");
 module.exports = (app) => {
     app.get('/live', (req, res) => {
         session.data[req.session.id] = { "name": req.session.name, "uid": req.session.uid, "allow": [] };
-        if(req.session.room && session.data[req.session.room] && session.data[req.params.uid].allow.includes(req.session.id)) {
+        if(req.session.room && session.data[req.session.room] && session.data[req.session.room].allow.includes(req.session.id)) {
             session.data[req.session.id].room = req.session.room;
         }
         res.render('live', {
